eebd7e49b2bbb75664d747df5a49dd72e35b27e6
Patrick Ng <PJLN07@gmail.com>
2019-07-12 20:02:23 -0400

    EDIT: methods for scraping data, starting game, and logic for restarting game

--- scraping_project.py
+++ scraping_project.py
@@ -1,36 +1,45 @@
 import requests
 from bs4 import BeautifulSoup
-from random import randint
-
-url = "http://quotes.toscrape.com/"
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
-quotes = soup.select('.quote')
+from random import choice
+from time import sleep
+# can delay function call with sleep(# of seconds)
 
 data = []
 
-for quote in quotes:
-    text = quote.find(class_="text").get_text()
-    name = quote.find('small').get_text()
-    link = quote.find('a')['href']
-    data.append({
-        "text": text,
-        "name": name,
-        "link": link
-    })
+def scrape_data(url):
+    print('Loading data...')
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    quotes = soup.select('.quote')
+
+    for quote in quotes:
+        text = quote.find(class_="text").get_text()
+        name = quote.find('small').get_text()
+        link = quote.find('a')['href']
+        data.append({
+            "text": text,
+            "name": name,
+            "link": link
+        })
+    print('Data loaded!')
 
 def start_game():
+    print('Starting game...')
     guesses = 4
-    quote_idx = randint(0, len(data))
-    selected_quote = data[quote_idx]
-    start = input(f"{selected_quote['text']}. Who said this quote?\n")
-    while guesses > 0:
-        if start != selected_quote['name']:
-            guesses -= 1
-            start = input('Nope. Try again!\n')
-        print("That's it, you won!")
-        return "That's it, you won!"
-    print("Sorry, you ran out of guesses...")
-    return "Sorry, you ran out of guesses..."
+    selected_quote = choice(data)
+    start = input(f"{selected_quote['text']}. Who said this quote? {guesses} guesses remaining\n")
+    while start != selected_quote['name']:
+        if guesses == 1:
+            print("Sorry ran out of guesses...")
+            replay = input("Would you like to play again?\n")
+            if replay.lower() in ['yes', 'y']:
+                return start_game()
+            print('Thanks for playing!')
+            return 'Thanks for playing!'
+        guesses -= 1
+        start = input(f'Nope. Try again! {guesses} guess(es) remaining\n')
+    print('That\"s it! You won!')
+    
 
+scrape_data("http://quotes.toscrape.com/")
 start_game()
