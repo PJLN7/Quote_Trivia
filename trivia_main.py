6bab07a892f37b6710a0120f120c02b61ace00b6
Patrick Ng <PJLN07@gmail.com>
2019-07-12 20:41:19 -0400

    ADD: functionality to save to/load from csv file

--- trivia_main.py
+++ trivia_main.py
@@ -1,19 +1,20 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 from random import choice
-from time import sleep
-# can delay function call with sleep(# of seconds)
-
-data = []
+from csv import reader, writer
 
 def scrape_data(url):
-    print('Loading data...')
+    # Using BeautifulSoup to scrape data from a commonly used website for scraping
+    print('Fetching data...')
     response = requests.get(url)
     soup = BeautifulSoup(response.text, 'html.parser')
     quotes = soup.select('.quote')
+    data = []
 
     for quote in quotes:
-        text = quote.find(class_="text").get_text()
+        unknown_chars_regex = re.compile(r"[“”]")
+        text = unknown_chars_regex.sub('', quote.find(class_="text").get_text())
         name = quote.find('small').get_text()
         link = quote.find('a')['href']
         data.append({
@@ -21,14 +22,31 @@ def scrape_data(url):
             "name": name,
             "link": link
         })
-    print('Data loaded!')
+    print('Data fetched!')
+    return data
+
+def save_data_to_file(data):
+    # Saving data scraped from website to a newly generated csv file
+    print('Saving data...')
+    with open("quotes_list.csv", "w", newline='') as file:
+        csv_writer = writer(file)
+        csv_writer.writerow(['text', 'name', 'link'])
+        for quote in data:
+            csv_writer.writerow([quote['text'], quote['name'], quote['link']])
+    print('Data saved!')
 
 def start_game():
+    # Reading from CSV file and starting game
+    quotes_list = []
+    with open('quotes_list.csv') as file:
+        csv_reader = reader(file)
+        quotes_list = list(csv_reader)[1::]
     print('Starting game...')
     guesses = 4
-    selected_quote = choice(data)
-    start = input(f"{selected_quote['text']}. Who said this quote? {guesses} guesses remaining\n")
-    while start != selected_quote['name']:
+    selected_quote = choice(quotes_list)
+    start = input(
+        f"{selected_quote[0]}. Who said this quote?\n{guesses} guesses remaining\n")
+    while start != selected_quote[1]:
         if guesses == 1:
             print("Sorry ran out of guesses...")
             replay = input("Would you like to play again?\n")
@@ -37,9 +55,8 @@ def start_game():
             print('Thanks for playing!')
             return 'Thanks for playing!'
         guesses -= 1
-        start = input(f'Nope. Try again! {guesses} guess(es) remaining\n')
+        start = input(f'Nope. Try again!\n{guesses} guess(es) remaining\n')
     print('That\"s it! You won!')
-    
 
-scrape_data("http://quotes.toscrape.com/")
+save_data_to_file(scrape_data("http://quotes.toscrape.com/"))
 start_game()
